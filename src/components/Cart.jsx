b19aa6b4503f21f51cfc50efa04468feda5e20be
suhail <shaikmsuhail02@gmail.com>
2022-11-12 19:33:30 +0530

    added remove functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,80 +5,83 @@ import Item from "./Item";
 import addedItems from "../util/cartItems";
 
 const Cart = () => {
-	// console.log(addedItems);
-	const [isLoad, setIsLoad] = useState(false);
-	const plusCounter = (index) => {
-		cartItems[index]["count"] = cartItems[index]["count"] + 1;
-		setIsLoad(!isLoad);
-	};
+  const [isLoad, setIsLoad] = useState(false);
+  const plusCounter = (index) => {
+    cartItems[index]["count"] = cartItems[index]["count"] + 1;
+    setIsLoad(!isLoad);
+  };
 
-	const minusCounter = (index) => {
-		if (cartItems[index]["count"] <= 1) {
-			cartItems[index]["count"] = 1;
-		} else {
-			cartItems[index]["count"] = cartItems[index]["count"] - 1;
-		}
-		setIsLoad(!isLoad);
-	};
+  const minusCounter = (index) => {
+    if (cartItems[index]["count"] <= 1) {
+      cartItems[index]["count"] = 1;
+    } else {
+      cartItems[index]["count"] = cartItems[index]["count"] - 1;
+    }
+    setIsLoad(!isLoad);
+  };
 
-	const uniqueIDs = [];
+  const uniqueIDs = [];
 
-	const cartItems = addedItems.filter((item) => {
-		const isDuplicate = uniqueIDs.includes(item.name);
+  let cartItems = addedItems.filter((item) => {
+    const isDuplicate = uniqueIDs.includes(item.name);
 
-		if (!isDuplicate) {
-			uniqueIDs.push(item.name);
-			return true;
-		}
+    if (!isDuplicate) {
+      uniqueIDs.push(item.name);
+      return true;
+    }
 
-		return false;
-	});
+    return false;
+  });
+  const [items, setItems] = useState(cartItems);
 
-	// console.log(cartItems);
+  const removeHandler = (name) => {
+    let newList = items.filter((i) => i.name !== name);
+    setItems(newList);
+  };
 
-	if (cartItems.length === 0) {
-		return <h2 className="no-items">No items added</h2>;
-	} else {
-		return (
-			<div className="cart-div">
-				{cartItems.map((item, index) => {
-					const { image, name, gen, flag, price, count } = item;
-					return (
-						<div className="cart-item">
-							<img className="cart-item-img" src={image} alt={`${name}-img`} />
-							<div className="cart-item-details">
-								<h2>{name}</h2>
-								<p>{gen}</p>
-								<p className="flags">{flag}</p>
-								<p>{price}</p>
-								<div className="quantity-setting">
-									<button
-										onClick={() => plusCounter(index)}
-										className="plus-btn"
-									>
-										+
-									</button>
-									<p className="count">{count}</p>
-									<button
-										onClick={() => minusCounter(index)}
-										className="minus-btn"
-									>
-										-
-									</button>
-								</div>
-								<button
-									// onClick={}
-									className="remove-btn"
-								>
-									remove
-								</button>
-							</div>
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
+  if (items.length === 0) {
+    return <h2 className="no-items">No items added</h2>;
+  } else {
+    return (
+      <div className="cart-div">
+        {items.map((item, index) => {
+          const { image, name, gen, flag, price, count } = item;
+          return (
+            <div key={name} className="cart-item">
+              <img className="cart-item-img" src={image} alt={`${name}-img`} />
+              <div className="cart-item-details">
+                <h2>{name}</h2>
+                <p>{gen}</p>
+                <p className="flags">{flag}</p>
+                <p>{price}</p>
+                <div className="quantity-setting">
+                  <button
+                    onClick={() => plusCounter(index)}
+                    className="plus-btn"
+                  >
+                    +
+                  </button>
+                  <p className="count">{count}</p>
+                  <button
+                    onClick={() => minusCounter(index)}
+                    className="minus-btn"
+                  >
+                    -
+                  </button>
+                </div>
+                <button
+                  onClick={() => removeHandler(name)}
+                  className="remove-btn"
+                >
+                  remove
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 export default Cart;
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,43 +3,48 @@ import Cart from "./Cart";
 import cartItems from "../util/cartItems.js";
 
 const Item = (item) => {
-	//   console.log(item);
-	const { image, name, gen, flag, price } = item;
-	const addHandler = (image, name, gen, flag, price) => {
-		const isItemPresentIndex = cartItems.findIndex(
-			(item) => item.name === name
-		);
-		if (isItemPresentIndex !== -1) {
-			cartItems[isItemPresentIndex]["count"] =
-				cartItems[isItemPresentIndex]["count"] + 1;
-		} else {
-			cartItems.push({
-				name: name,
-				image: image,
-				gen: gen,
-				flag: flag,
-				price: price,
-				count: 1,
-			});
-		}
-		// console.log(cartItems);
-	};
+  //   console.log(item);
+  const { image, name, gen, flag, price } = item;
+  const addHandler = (image, name, gen, flag, price, e) => {
+    let changeTextBack = () => {
+      e.target.innerHTML = "Add to cart";
+    };
+    e.target.innerText = "item added!";
+    setTimeout(changeTextBack, 500);
+    const isItemPresentIndex = cartItems.findIndex(
+      (item) => item.name === name
+    );
+    if (isItemPresentIndex !== -1) {
+      cartItems[isItemPresentIndex]["count"] =
+        cartItems[isItemPresentIndex]["count"] + 1;
+    } else {
+      cartItems.push({
+        name: name,
+        image: image,
+        gen: gen,
+        flag: flag,
+        price: price,
+        count: 1,
+      });
+    }
+    // console.log(cartItems);
+  };
 
-	return (
-		<div className="item">
-			<img className="item-image" src={image} alt="product-1" />
-			<h2>{name}</h2>
-			<p>{gen}</p>
-			<p className="flags">{flag}</p>
-			<p>{price}</p>
-			<button
-				onClick={() => addHandler(image, name, gen, flag, price)}
-				className="item-btn"
-			>
-				Add to cart
-			</button>
-		</div>
-	);
+  return (
+    <div className="item">
+      <img className="item-image" src={image} alt="product-1" />
+      <h2>{name}</h2>
+      <p>{gen}</p>
+      <p className="flags">{flag}</p>
+      <p>{price}</p>
+      <button
+        onClick={(e) => addHandler(image, name, gen, flag, price, e)}
+        className="item-btn"
+      >
+        Add to cart
+      </button>
+    </div>
+  );
 };
 
 export default Item;
